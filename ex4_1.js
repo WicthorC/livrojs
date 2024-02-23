b2d019698545bb966726814d11c7c208deb7aee3
Wicthor Costa <156452301+WicthorC@users.noreply.github.com>
2024-02-23 15:58:19 -0300

    Add files via upload

---
+++ ex4_1.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+frm.addEventListener("submit" , (e) => {
+e.preventDefault()
+const nome = frm.inNome.value 
+const nota1 = Number(frm.inNota1.value)
+const nota2 = Number(frm.inNota2.value)
+const media = (nota1 + nota2) / 2
+resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+
+if (media >= 7) {}
+
+
+})
